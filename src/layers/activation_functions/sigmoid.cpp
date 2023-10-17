1986d7a131e92ee48287413e9714aa80547d8ac3
trickybestia <trickybestia@gmail.com>
2023-10-17 21:24:00 +0300

    Do not recompute sigmoid value in backward pass

--- src/layers/activation_functions/sigmoid.cpp
+++ src/layers/activation_functions/sigmoid.cpp
@@ -2,10 +2,22 @@
 
 #include "sigmoid.hpp"
 
-TFloat Sigmoid::apply(TFloat x) const { return 1.0 / (1.0 + exp(-x)); }
+Matrix Sigmoid::forward(Matrix input) {
+  this->_output = Matrix(input.rows(), input.cols());
 
-TFloat Sigmoid::derivative(TFloat x) const {
-  TFloat value = this->apply(x);
+  for (size_t i = 0; i != input.size(); i++) {
+    this->_output(i) = 1.0 / (1.0 + exp(-input(i)));
+  }
 
-  return value * (1.0 - value);
+  return this->_output;
+}
+
+Matrix Sigmoid::previous_layer_error(const Matrix &layer_error) {
+  Matrix result(layer_error.rows(), layer_error.cols());
+
+  for (size_t i = 0; i != layer_error.size(); i++) {
+    result(i) = layer_error(i) * this->_output(i) * (1.0 - this->_output(i));
+  }
+
+  return result;
 }
--- src/layers/activation_functions/sigmoid.hpp
+++ src/layers/activation_functions/sigmoid.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
-#include "../simple_activation_function.hpp"
+#include "../activation_function.hpp"
 
-class Sigmoid : public SimpleActivationFunction {
+class Sigmoid : public ActivationFunction {
+private:
+  Matrix _output;
+
 public:
-  virtual TFloat apply(TFloat x) const override;
-  virtual TFloat derivative(TFloat x) const override;
+  virtual Matrix forward(Matrix input) override;
+
+  virtual Matrix previous_layer_error(const Matrix &layer_error) override;
 };
