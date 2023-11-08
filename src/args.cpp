60f3d07ea952baf005676c30047488930d95e056
trickybestia <trickybestia@gmail.com>
2023-11-08 13:44:40 +0300

    Add dots to CLI args descriptions

--- src/args.cpp
+++ src/args.cpp
@@ -10,44 +10,44 @@ using namespace std;
 Args parse_args(int argc, char **argv) {
   CmdLine cmdline("DIY neural network for digit recognition");
 
-  ValueArg<string> model_arg("m", "model", "Path to model file", false, "",
+  ValueArg<string> model_arg("m", "model", "Path to model file.", false, "",
                              "path", cmdline);
   ValueArg<string> dataset_arg("d", "dataset",
-                               "Path to MNIST Handwritten dataset root", false,
+                               "Path to MNIST Handwritten dataset root.", false,
                                "", "path", cmdline);
   ValueArg<string> dump_dataset_arg("", "dump-dataset",
-                                    "If present, dump dataset images to path",
+                                    "If present, dump dataset images to path.",
                                     false, "", "path");
   ValuesConstraint<string> optimizer_arg_constraint(
       {"SGD", "Adam", "Momentum"});
   ValueArg<string> optimizer_arg("o", "optimizer",
-                                 "Name of optimizer used in learning", false,
+                                 "Name of optimizer used in learning.", false,
                                  "", &optimizer_arg_constraint, cmdline);
   SwitchArg train_arg("t", "train", "Train model");
-  SwitchArg showcase_arg("s", "showcase", "Run in showcase mode");
+  SwitchArg showcase_arg("s", "showcase", "Run in showcase mode.");
   SwitchArg train_existing_model_arg("e", "train-existing-model",
-                                     "Train existing model", cmdline);
+                                     "Train existing model.", cmdline);
   SwitchArg train_with_dropout_arg("p", "train-with-dropout",
-                                   "Train model with dropout", cmdline);
-  SwitchArg compute_test_accuracy_arg("a", "compute-test-accuracy",
-                                      "Compute model accuracy on test dataset");
+                                   "Train model with dropout.", cmdline);
+  SwitchArg compute_test_accuracy_arg(
+      "a", "compute-test-accuracy", "Compute model accuracy on test dataset.");
   ValueArg<TFloat> learning_rate_arg("l", "learning-rate",
-                                     "Learning rate [DEFAULT: 0.01]", false,
+                                     "Learning rate. [DEFAULT: 0.01]", false,
                                      0.01, "float", cmdline);
   ValueArg<TFloat> random_parameter_min_arg(
       "", "random-parameter-min",
-      "Min value of randomized model parameter [DEFAULT: -0.3]", false, -0.3,
+      "Min value of randomized model parameter. [DEFAULT: -0.3]", false, -0.3,
       "float", cmdline);
   ValueArg<TFloat> random_parameter_max_arg(
       "", "random-parameter-max",
-      "Max value of randomized model parameter [DEFAULT: 0.3]", false, 0.3,
+      "Max value of randomized model parameter. [DEFAULT: 0.3]", false, 0.3,
       "float", cmdline);
   ValueArg<TFloat> beta1_arg("", "beta1",
                              "Beta hyperparameter for Momentum optimizer, or "
-                             "beta1 for Adam [DEFAULT: 0.9]",
+                             "beta1 for Adam. [DEFAULT: 0.9]",
                              false, 0.9, "float", cmdline);
   ValueArg<TFloat> beta2_arg(
-      "", "beta2", "Beta2 hyperparameter for Adam optimizer [DEFAULT: 0.999]",
+      "", "beta2", "Beta2 hyperparameter for Adam optimizer. [DEFAULT: 0.999]",
       false, 0.999, "float", cmdline);
 
   cmdline.xorAdd({&dump_dataset_arg, &train_arg, &showcase_arg,
